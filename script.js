3a917ab8662301068bfa06597430062b2f059cc0
DawidS2510 <d.sadowski@edu.salford.ac.uk>
2024-10-23 19:47:32 +0100

    Form testing

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ document.addEventListener('DOMContentLoaded', function() {
   
     animateFootball();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('#contact-form form');
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(form);
+      fetch(form.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          alert('Thanks for your message! I\'ll get back to you soon.');
+          form.reset();
+        } else {
+          alert('Oops! There was a problem submitting your form');
+        }
+      }).catch(error => {
+        alert('Oops! There was a problem submitting your form');
+      });
+    });
+  });
